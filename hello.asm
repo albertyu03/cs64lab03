b143ff810177a7ac4a4471e5f13f16a72ef8e79d
Albert Yu <albertyu@cs.ucsb.edu>
2023-02-04 15:19:56 -0800

    fixing for submission

--- hello.asm
+++ hello.asm
@@ -5,8 +5,9 @@
 .data
 	# TODO: Write your string definitions here
 	string1: .asciiz "Hello World 2023!\n"
-	string2: .asciiz "The magic number is 464!!!\n"
+	string2: .asciiz "The magic number is "
 	string3: .asciiz "Enter an integer:\n"
+	exclam: .asciiz "!!!\n"
 #Text Area (i.e. instructions)
 .text
 main:
@@ -16,12 +17,20 @@ main:
 	syscall
 	li $v0, 5
 	syscall
+	move $t0, $v0
 	#printing strings
 	la $a0, string1
 	li $v0, 4
 	syscall
 	la $a0, string2
 	syscall
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	li $v0, 4
+	la $a0, exclam
+	syscall
+
 	j exit
 
 exit:
